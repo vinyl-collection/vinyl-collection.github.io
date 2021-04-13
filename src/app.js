81317b1b62bcffa7dd4495a34335c8878ceb8022
Lachezar Tsekov <lchziy11@gmail.com>
2021-04-13 11:00:58 +0300

    app.js update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import { render } from '../node_modules/lit-html/lit-html.js'
+// import { render } from '../node_modules/lit-html/lit-html.js'
 import page from "//unpkg.com/page/page.mjs";
+import { render} from 'https://unpkg.com/lit-html?module';
+
 
 
 import { loginPage } from './views/login.js'
@@ -7,8 +9,7 @@ import { mainPage } from './views/dashboard.js'
 import { addPage } from './views/add.js'
 import { detailPage } from './views/details.js'
 import { editPage } from './views/edit.js'
-import { registerPage } from './views/register.js'
-import { logout } from './api/api.js';
+
 
 const main = document.querySelector('.container')
 
